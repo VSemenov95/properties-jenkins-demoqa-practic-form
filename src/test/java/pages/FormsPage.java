6f31ca8e39e44d043b10245ab2ca7886e1c468ba
homeWork_1.12 <semasvs18@gmail.com>
2026-02-22 15:57:31 +0300

    fixed dependencies

--- src/test/java/pages/FormsPage.java
+++ src/test/java/pages/FormsPage.java
@@ -3,15 +3,13 @@ package pages;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 
 public class FormsPage {
     private final SelenideElement
             practiceFormMenu = $("a[href='/automation-practice-form']");
 
-    public FormsPage openPracticeForm() {
+    public void openPracticeForm() {
         practiceFormMenu.click();
 
-        return this;
     }
 }
